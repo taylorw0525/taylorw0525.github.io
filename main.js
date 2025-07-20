98930ce54fde68061e3d9367dbc9fd9d0356d6b0
Taylor Webb <144720204+taylorw0525@users.noreply.github.com>
2025-07-20 19:28:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Ensure the script runs after the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed(".text", {
-        strings: ["AI / Machine Learning Engineer", "Data Scientist", "Software Engineer"],
+        strings: ["Software Engineer", "AI / Machine Learning", "Data Scientist"],
         typeSpeed: 70,  // Speed of typing effect
         backSpeed: 50,   // Speed of backspacing
         backDelay: 1000, // Pause before deleting
